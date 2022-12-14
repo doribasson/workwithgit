6ea4e01a4a4ec5b5fd5de6137e3db898bb4260f7
Dori basson <doribasson@h-MacBook-Air-sl-Dori.local>
2022-12-14 19:45:36 +0200

    feature1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Feature1 from "./feature1/Feature1";
+
 function App() {
   return (
     <div className="App">
-      <h1>First</h1>
+      <h1>
+        <Feature1 />
+      </h1>
     </div>
   );
 }
