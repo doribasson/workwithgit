a8754b264273d2b145fbae1e61e48436de4e6510
Dori basson <doribasson@h-MacBook-Air-sl-Dori.local>
2022-12-14 19:50:52 +0200

    feature2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Feature2 from "./feature2/Feature2";
+
 function App() {
   return (
     <div className="App">
-      <h1>First</h1>
+      <Feature2 />
     </div>
   );
 }
-
 export default App;
---
+++ src/feature2/Feature2.jsx
@@ -0,0 +1,5 @@
+function Feature2() {
+  return <h1>Feature2</h1>;
+}
+
+export default Feature2;
