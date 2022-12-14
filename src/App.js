673b3e8fa9a1e229272c4da97688a1b797016bba
Dori basson <dorib.api@gmail.com>
2022-12-14 20:55:01 +0200

    feature3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Feature2 from "./feature2/Feature2";
 import Feature1 from "./feature1/Feature1";
+import Feature3 from "./feature3/Feature3";
 
 function App() {
   return (
-    <div className="App">           
+    <div className="App">
       <Feature1 />
       <Feature2 />
+      <Feature3 />
     </div>
   );
 }
---
+++ src/feature3/Feature3.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function feature3() {
+  return <h1>feature3</h1>;
+}
+
+export default feature3;
