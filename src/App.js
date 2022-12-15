50a381792210f958da1bfd27afcca62543380b9b
Dori basson <dorib.api@gmail.com>
2022-12-15 11:06:36 +0200

    add feature 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Feature2 from "./feature2/Feature2";
 import Feature1 from "./feature1/Feature1";
 import Feature3 from "./feature3/Feature3";
 import Feature4 from "./feature4/Feature4";
+import Feature5 from "./feature5/Feature5";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Feature2 />
       <Feature3 />
       <Feature4 />
+      <Feature5 />
     </div>
   );
 }
---
+++ src/feature5/Feature5.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Feature5() {
+  return <h1>Feature5</h1>;
+}
+
+export default Feature5;
