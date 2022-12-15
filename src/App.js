d4432acb89780e54d1130a13e6258d3a2b873b6a
Dori basson <dorib.api@gmail.com>
2022-12-15 11:16:40 +0200

    add feature6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Feature2 from "./feature2/Feature2";
 import Feature1 from "./feature1/Feature1";
 import Feature3 from "./feature3/Feature3";
 import Feature4 from "./feature4/Feature4";
+import Feature6 from "./feature6/Feature6";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Feature2 />
       <Feature3 />
       <Feature4 />
+      <Feature6 />
     </div>
   );
 }
